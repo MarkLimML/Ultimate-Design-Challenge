2d9b1e40ad94e619716dddea42c993684f92580f
TetsuyaKanade <aaronjohn30@gmail.com>
2019-04-15 22:27:25 +0800

    Remove "Artists:

--- src/AddSong/WithArtist.java
+++ src/AddSong/WithArtist.java
@@ -1,48 +1,48 @@
-package AddSong;
-
-import Model.DatabaseConnector;
-
-import java.util.ArrayList;
-
-public class WithArtist implements Metadata
-{
-    /*
-        0 - name
-        1 - imagePath
-        2 - songPath
-        3 - album
-        4 - albumYear
-        5 - artist
-        6 - genre
-     */
-    private DatabaseConnector dbc;
-    private String name;
-    private String imgPath;
-    private String songPath;
-    private int user_id;
-    private String artist;
-
-    @Override
-    public void attachDB(DatabaseConnector dbc) {
-        this.dbc = dbc;
-    }
-
-    @Override
-    public void saveToDatabase(Song song) {
-        int artist_id;
-        name = song.getName();
-        artist = song.getArtist();
-        imgPath = song.getImgPath();
-        songPath = song.getSongPath();
-        user_id = song.getUser_id();
-
-        artist_id = dbc.getArtistIdFromName(artist);
-
-        if (artist_id == -1) {
-            dbc.createArtist(artist);
-            artist_id = dbc.getArtistIdFromName(artist);
-        }
-
-        dbc.createSongWArtist(name, songPath, artist_id, user_id);
-    }
-}
+//package AddSong;
+//
+//import Model.DatabaseConnector;
+//
+//import java.util.ArrayList;
+//
+//public class WithArtist implements Metadata
+//{
+//    /*
+//        0 - name
+//        1 - imagePath
+//        2 - songPath
+//        3 - album
+//        4 - albumYear
+//        5 - artist
+//        6 - genre
+//     */
+//    private DatabaseConnector dbc;
+//    private String name;
+//    private String imgPath;
+//    private String songPath;
+//    private int user_id;
+//    private String artist;
+//
+//    @Override
+//    public void attachDB(DatabaseConnector dbc) {
+//        this.dbc = dbc;
+//    }
+//
+//    @Override
+//    public void saveToDatabase(Song song) {
+//        int artist_id;
+//        name = song.getName();
+//        artist = song.getArtist();
+//        imgPath = song.getImgPath();
+//        songPath = song.getSongPath();
+//        user_id = song.getUser_id();
+//
+//        artist_id = dbc.getArtistIdFromName(artist);
+//
+//        if (artist_id == -1) {
+//            dbc.createArtist(artist);
+//            artist_id = dbc.getArtistIdFromName(artist);
+//        }
+//
+//        dbc.createSongWArtist(name, songPath, artist_id, user_id);
+//    }
+//}
