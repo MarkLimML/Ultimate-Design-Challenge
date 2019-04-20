977fc91777c6dafce260ea6053d8c9d7127ece84
MarkLimML <mark_lim@dlsu.edu.ph>
2019-04-21 00:16:11 +0800

    Added functionality

--- src/Profile/ProfileController.java
+++ src/Profile/ProfileController.java
@@ -6,14 +6,16 @@ import Model.User;
 import dashboard.ControllerAbstract;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.*;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.util.Callback;
 import makePlaylist.Playlist;
 
-import javax.swing.text.TableView;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -44,14 +46,19 @@ public class ProfileController extends ControllerAbstract {
     @FXML
     public Pane playlisttablepane;
 
-    private javafx.scene.control.TableView<User> UserTable;
-    private TableColumn<User, String> colUsers;
-    private javafx.scene.control.TableView<User> SongTable;
-    private TableColumn<User, String> colArtist;
-    private TableColumn<User, String> colListener;
+    private TableView<Song> SongTable;
+    private TableColumn<Song, String> colSong;
+    private TableView<Playlist> PlaylistTable;
+    private TableColumn<Playlist, String> colPlaylist;
+    private TableView<User> UserTable;
+    private TableColumn<User, String> colUser;
+    private TableColumn<User, String> colType;
+    private ArrayList<BooleanProperty> selectedRowList;
+    Callback<Integer,ObservableValue<Boolean>> colCbxState;
 
     private ArrayList<User> users;
-    private ArrayList<Playlist> playlists;
+    private ObservableList<Song> songList;
+    private ObservableList<Playlist> playlists;
 
     private User displayedUser;
 
@@ -73,17 +80,43 @@ public class ProfileController extends ControllerAbstract {
         if(!username.getText().equals(ModelAbstract.getUser().getUsername()))
             isFollowed.setOpacity(1);
 
+
     }
 
-    public void getfollowing() {
+    private void setTableViewColumns() {
+        colUser = new TableColumn<User, String>("Name");
+        colType = new TableColumn<User, String>("Type");
+        colSong = new TableColumn<Song, String>("Favorite Song");
+        colPlaylist = new TableColumn<Playlist, String>("Playlists");
 
+        colUser.setMinWidth(50);
+        colType.setMinWidth(50);
+        colSong.setMinWidth(350);
+        colPlaylist.setMinWidth(350);
+
+
+        colUser.setCellValueFactory(new PropertyValueFactory<>("username"));
+        colType.setCellValueFactory(new PropertyValueFactory<>("isartist"));
+        colSong.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colPlaylist.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        colUser.setEditable(true);
+        colType.setEditable(true);
+        colSong.setEditable(false);
+        colPlaylist.setEditable(false);
+
+
+
+        SongTable.setFixedCellSize(60.0);
+        SongTable.setItems(songList);
+        SongTable.getColumns().addAll(colSong);
     }
 
     public void setIsFollowed(MouseEvent e) {
 
     }
 
-    public void callPreviousScreen(javafx.scene.input.MouseEvent e)  throws IOException
+    public void callPreviousScreen(MouseEvent e)  throws IOException
     {
         this.setScene(newProfilePane.getScene());
         this.switchScene(this.getScreenUrls()[0]);
